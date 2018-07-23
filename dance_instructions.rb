11fd598e20c362c3c70f7b420e176a54f6e10191
raynapatel <raynapatelpdx@gmail.com>
2018-07-23 23:35:36 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,7 +1,42 @@
 
 # Code your methods below
-def proper stance
+def proper_stance
   puts "Plant legs far apart"
   puts "Bend knees slightly"
   puts "Keep posture loose"
-end
+end 
+
+def base_footwork
+  puts "Lift right foot."
+  puts "Return right foot to the ground."
+  puts "Finishing with a small skip-step backward."
+end 
+def skip_step 
+  puts "Lower left foot to the ground"
+  puts "Bounce left foot back up slightly, kicking it a few inches back"
+  puts "Right, Left, Right and Right"
+  puts "Lower right foot to the ground." 
+  puts "Bounce right foot back up slightly, kicking it a few inches back." 
+  puts "Right, Left, Right and Right."
+end 
+def bob_the_reins
+  puts "Holding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over you left and hold them together"
+  puts "Lift your arms up and dowm in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times"
+end 
+def lasso 
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left." 
+  puts "Lift right arm up and point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end 
+def bust_a_move
+  puts "Be cool"
+  puts "Be snazzy"
+end 
+
+def gangnam_style
+  proper_stance
+  bust_a_move
+  base_footwork
+  bob_the_reins
+  skip_step
